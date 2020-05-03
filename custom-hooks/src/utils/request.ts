bd658a57de2d834c5fc0a1a925ef60d59f469b1d
mateja <mateja176@gmail.com>
2020-05-03 18:08:28 +0200

    fix(custom-hooks/utils/request): adjusts mapper

--- custom-hooks/src/utils/request.ts
+++ custom-hooks/src/utils/request.ts
@@ -2,4 +2,9 @@ import { RequestData } from '../models/request';
 
 export const getRequestResult = <R>(initialResult: R) => (
   data: RequestData<R>,
-) => (data === 'initial' ? initialResult : data === 'loading' ? null : null);
+) =>
+  data === 'initial'
+    ? initialResult
+    : data === 'loading' || data instanceof Error
+    ? null
+    : data;
