c0d6571eab6ac9277cfe122e32b167bd88993dfc
mateja <mateja176@gmail.com>
2020-05-07 00:27:30 +0200

    feat(custom/hooks/components/Story): removes unused id and formats file

--- custom-hooks/src/components/Story.tsx
+++ custom-hooks/src/components/Story.tsx
@@ -3,14 +3,7 @@ import { IStory } from '../models';
 
 export interface StoryProps extends IStory {}
 
-export const Story: React.FC<StoryProps> = ({
-  id,
-  title,
-  url,
-  type,
-  time,
-  by,
-}) => {
+export const Story: React.FC<StoryProps> = ({ title, url, type, time, by }) => {
   const date = new Date(time);
   const timeString = date.toLocaleTimeString();
   const dateString = date.toLocaleDateString();
